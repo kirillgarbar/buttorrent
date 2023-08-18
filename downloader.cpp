fc55c7ed0da82a7d3c5b7342932e2bed3c2cb5ad
kirillgarbar <st087492@student.spbu.ru>
2023-08-18 15:45:23 +0300

    Downloader downloads file

--- downloader.cpp
+++ downloader.cpp
@@ -15,9 +15,15 @@
 
 using namespace std;
 
+#define BlockSize 16384
+
 TorrentDownloader::TorrentDownloader(std::string fileName) {
     torrentFile = Decoder::getTorrentFile(fileName);
     peerId = generatePeerId();
+    piece = 0;
+    //Delete file contents if already exists
+    downloadedFile.open(torrentFile.Name, std::ofstream::out | std::ofstream::trunc);
+    downloadedFile.close();
 }
 
 std::string TorrentDownloader::generatePeerId() {
@@ -35,10 +41,83 @@ std::string TorrentDownloader::generatePeerId() {
 
 chrono::steady_clock::time_point getTimestamp() { return chrono::steady_clock::now(); }
 
+void TorrentDownloader::downloadPiece(Peer& peer, int length, int sock) {
+    //Number of blocks of BlockSize
+    //Most of the time PieceLength will be divisible by BlockSize
+    int totalFullBlocks = length / BlockSize;
+    int lastBlockLength = length % BlockSize == 0 ? BlockSize : length % BlockSize;
+    int totalBlocks = lastBlockLength < BlockSize ? totalFullBlocks + 1 : totalFullBlocks;
+    int block = 0;
+    int offset = 0;
+    std::stringstream pieceString;
+
+    while (block < totalBlocks) {
+        BitTorrentMessage message = PeerConnector::receiveMessage(sock);
+        switch (message.getId()) {
+            case MessageId::unchoke:
+                choke = false;
+                break;
+
+            case MessageId::choke:
+                choke = true;
+                break;
+
+            case MessageId::have:
+            {
+                int pieceIndex = bytesToInt(message.getPayload());
+                setBit(bitField, pieceIndex);
+                break;
+            }
+            
+            case MessageId::piece:
+            {
+                requestSent = false;
+                std::string payload = message.getPayload();
+                int index = bytesToInt(payload.substr(0, 4));
+                int begin = bytesToInt(payload.substr(4, 4));
+                if (index == piece) {
+                    if (begin == offset) { 
+                        pieceString << payload.substr(8); 
+                        offset += payload.size() - 8;
+                        block++;
+                    }
+                }
+                break;
+            }
+
+            default:
+                break;
+        }
+        if (block < totalBlocks && !requestSent && !choke) {
+            requestSent = true;
+            if (block == totalBlocks - 1) PeerConnector::requestPiece(piece, offset, lastBlockLength, sock);
+            else PeerConnector::requestPiece(piece, offset, BlockSize, sock);
+        }
+    }
+    string downloadedPiece = pieceString.str();
+    string pieceHash = hexDecode(Decoder::sha1(downloadedPiece));
+
+    cout << requestSent << !(pieceHash == torrentFile.PieceHashes[piece]) << endl;
+    cout << pieceHash << "     " << torrentFile.PieceHashes[piece] << endl;
+
+    //Piece downloaded successfully
+    if (pieceHash == torrentFile.PieceHashes[piece])  { 
+        this->piece++;
+        downloadedFile.open(torrentFile.Name, std::ofstream::out | std::ofstream::app);
+        downloadedFile << downloadedPiece;
+        downloadedFile.close();
+    }
+}
+
+void closeSocket(int sock) {
+    if (sock > -1) close(sock);
+}
+
 int TorrentDownloader::download() {
-    int progress = 0;
-    
-    while (progress < 100) {
+    static int totalPieces = torrentFile.PieceHashes.size();
+    static int lastPieceSize = torrentFile.Length - (totalPieces - 1) * torrentFile.PieceLength;
+
+    while (piece < totalPieces) {
         peers = PeerRetriever::retrievePeers(torrentFile, peerId);
         auto lastRetrieved = getTimestamp();
         int retrieveInterval = peers.interval * 1000;
@@ -47,18 +126,25 @@ int TorrentDownloader::download() {
         while (chrono::duration_cast<chrono::milliseconds>(getTimestamp() - lastRetrieved).count() < retrieveInterval 
         && peer < peers.peers.size())
         {
-            int sockfd = PeerConnector::handshake(this->peers.peers[peer], this->torrentFile.InfoHash, this->peerId);
-            if (sockfd > -1) {
-                string bitField = PeerConnector::receiveBitField(sockfd);
-                PeerConnector::interested(sockfd);
-                cout << bitField.size() << endl;
-                //downloadPiece(peers.peers[peer], sockfd);
-                close(sockfd);
+            int sockfd = -1;
+            try {
+                sockfd = PeerConnector::handshake(this->peers.peers[peer], this->torrentFile.InfoHash, this->peerId);
+                bitField = PeerConnector::receiveBitField(sockfd);
+
+                //If peer has interesting piece
+                while (getBit(bitField, piece)) {
+                    PeerConnector::interested(sockfd);
+                    if (piece == totalPieces - 1) downloadPiece(peers.peers[peer], lastPieceSize, sockfd);
+                    else downloadPiece(peers.peers[peer], torrentFile.PieceLength, sockfd);
+                }
+            } catch (runtime_error& e) {
+                //Failed to connect, try another peer
             }
             peer++;
-            cout << to_string(chrono::duration_cast<chrono::milliseconds>(getTimestamp() - lastRetrieved).count()) << endl;
+            choke = true;
+            requestSent = false;
+            closeSocket(sockfd);
         }
-        progress += 10;
     }
     return 0;
 }
--- downloader.hpp
+++ downloader.hpp
@@ -11,9 +11,18 @@ private:
     RetrievedPeers peers;
     std::string peerId;
 
+    int piece = 0;
+    bool choke = true;
+    bool interested = false;
+    bool requestSent = false;
+    std::string bitField;
+
+    std::ofstream downloadedFile;
+
     std::string generatePeerId();    
 public:
     TorrentDownloader(std::string fileName);
 
     int download();
+    void downloadPiece(Peer& peer, int length, int sock);
 };
